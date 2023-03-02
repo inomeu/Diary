142925cb6a3f97b9a83155aaddb305a01c2bad9b
Mei Inoue <inomei.0314@gmail.com>
2023-03-02 22:00:50 +0900

    [Add] Add File

---
+++ Diary/DiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiaryApp.swift
+//  Diary
+//
+//  Created by inoue mei on 2023/03/02.
+//
+
+import SwiftUI
+
+@main
+struct DiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
