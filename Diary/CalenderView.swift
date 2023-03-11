42df6046cf3939a7e21da0624c4fa92a2a79189c
Mei Inoue <inomei.0314@gmail.com>
2023-03-11 17:16:04 +0900

    Add CalenderView

---
+++ Diary/CalenderView.swift
@@ -0,0 +1,20 @@
+//
+//  CalenderView.swift
+//  Diary
+//
+//  Created by inoue mei on 2023/03/11.
+//
+
+import SwiftUI
+
+struct CalenderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CalenderView_Previews: PreviewProvider {
+    static var previews: some View {
+        CalenderView()
+    }
+}
